4dac1d01f0a16194838b5b87ce22916e0359c2b2
Christian Paul <christiansamuelpaul@gmail.com>
2016-03-10 19:45:21 -0500

    added comments

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 var express = require('express');
 var app = express();
-app.enable('trust proxy');
-
 var mongoose = require('mongoose');
+
+app.enable('trust proxy');
+var port = process.env.PORT || 3000;
+
+// mongoose initialization
 mongoose.connect('mongodb://christ:please@ds064748.mlab.com:64748/addressbook');
-
 var db = mongoose.connection;
-
 var urlSchema = new mongoose.Schema({
     id: Number,
     longurl: String
 });
-
 var Url = mongoose.model('Url', urlSchema);
 
-var port = process.env.PORT || 3000;
-
-var valid = /^(ftp|http|https):\/\/[^ "]+$/;
-
-app.get('/new/*', function(req, res) {
+app.get('/new/*', function(req, res) { 
     
     var longurl = req.originalUrl.split('/new/')[1];
+    var valid = /^(ftp|http|https):\/\/[^ "]+$/;
 
-    /*longurl doesn't have 'http://www.*.*' || 'https://www.*.*')*/
+    // validates url, if fails, send json error object
     if(!valid.test(longurl)) {
         res.json({ error: 'improper url format' });
-        console.log('longurl = ' + longurl);
     }
     else {
         Url.count({}, function(err, item) {
+            // count the number of documents in the collection
             var count = item;
-            console.log(count + ' items inside');
-
+            
+            // makes a new document with id number = # of docs
+            // and longurl = url user just entered
             var newurl = new Url({
                 id: count,
                 longurl: longurl
             });
-
+            
+            // save document
             newurl.save(function(err) {
                 if(err) {
                     console.log('error');
@@ -51,20 +50,20 @@ app.get('/new/*', function(req, res) {
 
             // for deployment
             res.json({ longurl: longurl, shorturl: req.hostname + '/' + count });
-
         });
     }
 });
 
+// these are the paths corresponding to shortened urls
 app.get('/:id', function(req, res) {
     
+    // find a document whose id matches the url :id
     Url.findOne({ id: req.params.id }, function(err, item) {
         if(err) {
             return console.log(err);
         }
+        // redirect to that document's longurl link
         else {
-            console.log(item);
-            console.log(item.longurl);
             res.redirect(item.longurl)
         }
     });
